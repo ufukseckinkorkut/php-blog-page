ace845c3e3c6f45286b98a2ae164796b9fcbc241
usk <uskn2004@gmail.com>
2025-05-30 13:55:59 +0300

    created mysql database connection

---
+++ database.php
@@ -0,0 +1,17 @@
+<?php
+
+    $db_server = "localhost";
+    $db_user = "root";
+    $db_password = "";
+    $db_name = "blog_db";
+    $connection = "";
+
+    $connection = mysqli_connect($db_server, $db_user, $db_password, $db_name);
+
+
+     /* if ($connection) {
+        echo "you are connected";
+    }   */ 
+
+
+?>
